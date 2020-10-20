ba560d158e63720e3bd803be98df9d98e180fb51
ViolanteCodes <maria@mariadong.com>
2020-10-20 14:27:51 -0400

    collect static first run

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.messages',
     'blog.apps.BlogConfig',
 ]
 
@@ -130,4 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+]
