b07ee3735f377e1d6e1936fb8b954a321ca2abce
ViolanteCodes <maria@mariadong.com>
2020-10-25 16:12:39 -0400

    Added view and template for condensed works

--- shorts/urls.py
+++ shorts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.shorts, name='shorts'),
+    path('condensed/', views.condensed, name='condensed')
 ]
 
--- shorts/views.py
+++ shorts/views.py
@@ -7,3 +7,8 @@ def shorts(request):
     unpubbed = Piece.objects.filter(published_date__isnull=True).order_by('-created_date')
     pubbed = Piece.objects.filter(published_date__isnull=False).order_by('-published_date')
     return render(request, 'shorts/shorts.html', {'unpubbed': unpubbed, 'pubbed':pubbed})
+
+def condensed(request):
+    unpubbed = Piece.objects.filter(published_date__isnull=True).order_by('-created_date')
+    pubbed = Piece.objects.filter(published_date__isnull=False).order_by('-published_date')
+    return render(request, 'shorts/condensed.html', {'unpubbed': unpubbed, 'pubbed':pubbed})
