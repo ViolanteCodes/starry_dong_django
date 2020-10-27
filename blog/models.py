eec161e26e0e86a8c17b52d18a7da0c2d1b6c9a6
ViolanteCodes <maria@mariadong.com>
2020-10-27 01:46:23 -0400

    Blog Tag model added, blog tags added to detail page

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,24 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class Tag(models.Model):
+    """A blog tag"""
+    name = models.CharField(max_length=200)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
+    """A blog post"""
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    tags = models.ManyToManyField('Tag', related_name ='posts')
 
     def publish(self):
         self.published_date = timezone.now()
