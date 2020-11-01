d4d3f886a3d20d51366838a2a2acd9b3d7d5b858
ViolanteCodes <maria@mariadong.com>
2020-11-01 00:49:37 -0400

    updated mysite.setting

--- mysite/urls.py
+++ mysite/urls.py
@@ -38,4 +38,3 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
  
  
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
