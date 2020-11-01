419009e1aa27355b5f3c2dc7b0c4a93600f40433
ViolanteCodes <maria@mariadong.com>
2020-11-01 03:54:53 -0500

    revert to url field

--- shorts/models.py
+++ shorts/models.py
@@ -47,7 +47,7 @@ class Piece(models.Model):
     created_date = models.DateField(blank=True, null=True)
     pending_date = models.DateField(blank=True, null=True)
     published_date = models.DateField(blank=True, null=True)
-    cover = models.ImageField(blank=True, null=True, upload_to='covers')
+    cover = models.URLField(blank=True, null=True)
 
     def publish(self):
         self.save()
