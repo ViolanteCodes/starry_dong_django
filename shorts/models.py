c8a0128404ef1a766bdfc1681104dd26591a7545
ViolanteCodes <maria@mviolante.com>
2021-04-10 17:43:27 -0400

    removed piece field from short fiction and made migrations

--- shorts/models.py
+++ shorts/models.py
@@ -47,7 +47,6 @@ class Piece(models.Model):
     created_date = models.DateField(blank=True, null=True)
     pending_date = models.DateField(blank=True, null=True)
     published_date = models.DateField(blank=True, null=True)
-    cover = models.URLField(blank=True, null=True)
     cover_upload = models.ImageField(blank=True, null=True)
 
     def publish(self):
