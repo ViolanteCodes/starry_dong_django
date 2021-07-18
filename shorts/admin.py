e30b441e51bfcc188867c03449ea52e140a559fb
ViolanteCodes <maria@mviolante.com>
2021-07-18 10:30:16 -0400

    added bootstrap 5

--- shorts/admin.py
+++ shorts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Publisher, Piece, Review, Genre
+from .models import Publisher, Piece, Review, Genre, Category
 
 admin.site.register(Publisher)
 admin.site.register(Piece)
 admin.site.register(Review)
 admin.site.register(Genre)
+admin.site.register(Category)
--- shorts/models.py
+++ shorts/models.py
@@ -5,9 +5,22 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    sort_order = models.IntegerField(null=True, blank=True, unique=True)
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all Reivews
+        context['review_list'] = Review.objects.all()
+        return context
+
+    def __str__(self):
+        return self.name
+
 class Genre(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, null=True, blank=True)
 
     def publish(self):
         self.save()
@@ -28,13 +41,7 @@ class Publisher(models.Model):
 class Piece(models.Model):
     """A representation of a piece of fiction."""
     author = 'Maria Dong'
-    PIECE_TYPE_CHOICES = [
-        ('story', 'Short Story'),
-        ('poem', 'Poem'),
-        ('essay', 'Essay'),
-        ('article', 'Article')
-    ]
-    piece_type = models.CharField(max_length=200, choices = PIECE_TYPE_CHOICES, default='story')
+    category = models.ForeignKey('Category', related_name='pieces', on_delete="models.CASCADE", null=True)
     genre = models.ManyToManyField('Genre', related_name = 'pieces')
     title = models.CharField(max_length=200)
     word_count = models.IntegerField()
@@ -42,12 +49,15 @@ class Piece(models.Model):
     issue = models.CharField(max_length=200, blank=True)
     piece_url = models.URLField(blank=True)
     audio_url = models.URLField(blank=True)
-    pull_quote = models.TextField()
+    pull_quote = models.TextField(blank=True)
     tags = models.CharField(max_length=200, blank=True)
     created_date = models.DateField(blank=True, null=True)
     pending_date = models.DateField(blank=True, null=True)
     published_date = models.DateField(blank=True, null=True)
     cover_upload = models.ImageField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['-pending_date', '-published_date']
 
     def publish(self):
         self.save()
