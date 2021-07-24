c3c69a6f786a7b879412521a2cef6e41573d031f
ViolanteCodes <maria@mviolante.com>
2021-07-24 09:36:41 -0400

    redid bio to autopopulate publishers

--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from shorts.models import Publisher, Piece
 
 def landing(request):
     return render(request, 'landing.html')
+
 def about(request):
-    return render(request, 'about.html')
+    """View to create the about me section"""
+    published_list = Publisher.objects.filter(piece__published_date__isnull=False).order_by('publisher_name').distinct()
+    pending_list = Publisher.objects.filter(piece__published_date__isnull=True).order_by('publisher_name').distinct()
+    return render(request, 'about.html', {'published_list':published_list, 'pending_list':pending_list})
+
 def editorial(request):
     return render(request, 'editorial.html')
 def fiction(request):
