9bee20e8b68c5f759ccfa2956d213a995ed15a05
ViolanteCodes <maria@mviolante.com>
2021-08-09 18:57:47 -0400

    change body field

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     """A blog post"""
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = RichTextField(blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     tags = models.ManyToManyField('Tag', related_name ='posts')
