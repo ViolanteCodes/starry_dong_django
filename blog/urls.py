3f8df67db73d4735d99b999708ff9e0528ad3083
ViolanteCodes <maria@mviolante.com>
2021-08-15 11:30:26 -0400

    switched from blog app to mailchimp newsletter archive

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.mailchimp_post_list, name='post_list'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
